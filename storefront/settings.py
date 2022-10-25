840d4677c38ed2dd32e8eb00004a01b151d0ff28
unknown <hamimulhasan@gmail.com>
2022-10-25 20:07:23 +0600

    added static

--- storefront/settings.py
+++ storefront/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
