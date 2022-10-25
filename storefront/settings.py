6622a15e5210caf990f14b98f6a048cdcd0ad09d
unknown <hamimulhasan@gmail.com>
2022-10-25 20:52:20 +0600

    allowed host

--- storefront/settings.py
+++ storefront/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
